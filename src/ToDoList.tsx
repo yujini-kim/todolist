1507ce639d528226d58e65a4ba551191ac349389
yujin <kingyujin56@gmail.com>
2025-03-30 17:49:41 +0900

    feat: 에러메세지 표시
    - 인풋태그의 규칙에 어긋날 경우, 에러메세지 표시
    
    관련 이슈:

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,25 +1,59 @@
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+type IFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  CheckingPassword: string;
+  username?: string;
+};
 
 function ToDoList() {
-  const [toDo, setToDo] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>();
 
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setToDo(value);
+  const onValid = (data: IFormData) => {
+    console.log('data', data);
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(toDo);
-  };
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register('email', {
+            required: '이메일을 입력해주세요',
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: 'Only naver.com emails allowed',
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors.email?.message}</span>
+
+        <input
+          {...register('username', {
+            required: '유저네임을 입력해주세요',
+            minLength: {
+              value: 10,
+              message: 'Username must be at least 10 characters',
+            },
+          })}
+          placeholder="Username"
+        />
+        <span>{errors.username?.message}</span>
+
         <button>Add</button>
       </form>
     </div>
   );
 }
+
 export default ToDoList;
