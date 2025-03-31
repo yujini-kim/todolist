b0d4d380badb5aad83d00428601e659c52632324
yujin <kingyujin56@gmail.com>
2025-03-31 21:43:48 +0900

    feat: 카테고리별로 투두리스트 분리
    -
    
    관련 이슈:

--- src/component/ToDoList.tsx
+++ src/component/ToDoList.tsx
@@ -1,21 +1,37 @@
 import { useRecoilValue } from 'recoil';
 import CreateTodo from './CreateTodo';
-import { toDoState } from '../atoms';
+import { toDoSelector, toDoState } from '../atoms';
 import Todo from './ToDo';
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateTodo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((todo) => (
+        {toDo.map((todo) => (
           <Todo key={todo.id} {...todo} />
         ))}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((todo) => (
+          <Todo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((todo) => (
+          <Todo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
